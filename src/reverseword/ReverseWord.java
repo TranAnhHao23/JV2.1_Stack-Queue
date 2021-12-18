60f266eba7362cde8dc782a06ad8d199f97866bc
TranAnhHao23 <Trananhhaonuce@gmail.com>
2021-12-18 18:06:49 +0700

    Add file

---
+++ src/reverseword/ReverseWord.java
@@ -0,0 +1,24 @@
+package reverseword;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ReverseWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập chuỗi muốn đảo ngược: ");
+        String mWord = sc.nextLine();
+        Stack<String> wStack = new Stack<>();
+        char[] ch = mWord.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            wStack.push(String.valueOf(ch[i]));
+        }
+        String newWord = "";
+        while (wStack.size() != 0){
+            newWord += wStack.pop();
+        }
+        System.out.println(newWord);
+    }
+
+
+}
